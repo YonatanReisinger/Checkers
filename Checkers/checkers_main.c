10afe85fafc9aad58d2c613668f9f77d1d825f78
YonatanReisinger <yonatanrisinger33@gmail.com>
2023-05-15 14:39:27 +0300

    new branch

--- Checkers/checkers_main.c
+++ Checkers/checkers_main.c
@@ -5,7 +5,7 @@ void main()
 	Board board;
 	checkersPos pos = {5,4};
 	initBoard(board);
-	board[2][1] = EMPTY;
+	/*board[2][1] = EMPTY;
 	board[3][2] = PLAYER_1;
 	board[5][2] = EMPTY;
 	board[4][3] = PLAYER_2;
@@ -13,11 +13,11 @@ void main()
 	board[4][3] = PLAYER_1;
 	board[3][2] = EMPTY;
 	board[2][1] = PLAYER_1;
-	board[1][0] = EMPTY;
+	board[1][0] = EMPTY;*/
 
 
-	SingleSourceMovesTree* tr = FindSingleSourceMoves(board, &pos);
-	
+	multipleSourceMovesList* msmLst = FindAllPossiblePlayerMoves(board, PLAYER_1);
+
 	printBoard(board);
 	//printBoard(tr->source->nextMoves[0]->board);
 }
